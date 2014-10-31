b7b919fc3471ebb300ab9f5bf67335bfc6cfde9b
gideonparanoid <gideon.jones@ntlworld.com>
2014-10-31 18:39:24 +0000

    emissive lighting

---
+++ src/13/js/class.js
@@ -0,0 +1,416 @@
+/**
+ * @author (gij2) gideon mw jones
+ * @created 2014-10-25
+ */
+
+'use strict';
+
+
+/**
+ * rudimentary class for creating drawable items & drawing them in webgl, constructs the item as a 
+ *    series of elements & positions them using the animation parameter
+ *
+ * @param   vertices       array of numbers as vertex descriptions ([x1, y1, z1, x2, y2, z2])
+ * @param   vertexIndices  array of numbers as indices in the vertices describing elements
+ * @param   normals        array of numbers as element normals
+ * @param   textureURL     string path to the texture image
+ * @param   textureCoord   array of numbers as indices in the vertices describing texture coords
+ * @param   animation      function moving the item's position for drawing, can be used to animate
+ * @param   lighting       function to set up any lighting parameters specifically for this item
+ */
+function ItemElements(vertices, vertexIndices, normals, textureURL, textureCoord, animation, 
+   lighting) {
+
+   this.vertices = new Float32Array(vertices);
+   
+   this.vertexIndices = new Uint16Array(vertexIndices);   
+   
+   this.normals = new Float32Array(normals);
+
+   this.texture = gl.createTexture();
+   this.texture.image = new Image();
+   this.texture.image.src = textureURL;
+
+   // passing texture to function
+   var _texture = this.texture;
+
+   // finishing initialisation after loading resource
+   this.texture.image.onload = function() {
+      gl.bindTexture(gl.TEXTURE_2D, _texture);
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, _texture.image);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+      gl.generateMipmap(gl.TEXTURE_2D);
+      gl.bindTexture(gl.TEXTURE_2D, null);
+   }
+
+   this.textureCoord = new Float32Array(textureCoord);
+
+   this.animation = animation;
+
+   this.lighting = lighting;
+
+   this.bufferVertices = gl.createBuffer();
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferVertices);
+   gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.STATIC_DRAW);
+   
+   this.bufferVertexIndices = gl.createBuffer();
+   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufferVertexIndices);
+   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndices, gl.STATIC_DRAW);
+
+   this.bufferNormals = gl.createBuffer();
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferNormals);
+   gl.bufferData(gl.ARRAY_BUFFER, this.normals, gl.STATIC_DRAW);
+
+   this.bufferTextureCoord = gl.createBuffer();
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferTextureCoord);
+   gl.bufferData(gl.ARRAY_BUFFER, this.textureCoord, gl.STATIC_DRAW);
+}
+
+
+ItemElements.prototype.draw = function() {
+   this.lighting();
+   this.animation();
+
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferVertices);
+   gl.vertexAttribPointer(program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufferVertexIndices);
+
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferNormals);
+   gl.vertexAttribPointer(program.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+   
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferTextureCoord);
+   gl.vertexAttribPointer(program.textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+   gl.activeTexture(gl.TEXTURE0);
+   gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
+   setMatrixUniforms();
+   gl.drawElements(gl.TRIANGLES, this.vertexIndices.length, gl.UNSIGNED_SHORT, 0);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// shapes
+
+
+/**
+ * creates a sphere centring on the origin
+ * @super   ItemElements
+ * @param   radius         number radius of the circle to create
+ * @param   textureURL     @see ItemElements
+ * @param   animation      @see ItemElements
+ * @param   lighting       @see ItemElements
+ */
+function Sphere(radius, textureURL, animation, lighting) {
+   var bands = 30, 
+      vertices = [], 
+      normals = [], 
+      textureCoord = [];
+
+   // latitudes
+   for (var slice = 0; slice <= bands; slice++) {
+      var theta = slice * Math.PI / bands,
+          sinTheta = Math.sin(theta),
+          cosTheta = Math.cos(theta);
+
+      // longitudes
+      for (var arc = 0; arc <= bands; arc++) {
+         var phi = arc * 2 * Math.PI / bands,
+            sinPhi = Math.sin(phi),
+            cosPhi = Math.cos(phi);
+
+         var x = cosPhi * sinTheta,
+            y = cosTheta,
+            z = sinPhi * sinTheta,
+            u = 1 - (arc / bands),
+            v = 1 - (slice / bands);
+
+         vertices.push(radius * x, radius * y, radius * z);
+         textureCoord.push(u, v);
+         normals.push(x, y, z);
+      }
+   }
+
+   var vertexIndices = [];
+   for (var slice = 0; slice < bands; slice++) {
+      for (var arc = 0; arc < bands; arc++) {
+         var first = (slice * (bands + 1)) + arc,
+            second = first + bands + 1;
+
+         vertexIndices.push(first, second, first + 1, second, second + 1, first + 1);
+      }
+   }
+
+   ItemElements.call(this, vertices, vertexIndices, normals, textureURL, textureCoord, animation, 
+         lighting);
+}
+
+Sphere.prototype = Object.create(ItemElements.prototype);
+Sphere.prototype.constructor = Sphere; 
+
+
+
+/**
+ * creates a cube centring on the origin
+ * @super   ItemElements
+ * @param   size           number length of the sides of the cube
+ * @param   textureURL     @see ItemElements
+ * @param   animation      @see ItemElements
+ * @param   lighting       @see ItemElements
+ */
+function Cube(size, textureURL, animation, lighting) {
+   var s = size / 2;
+
+   ItemElements.call(this, [ // vertices
+        -s, -s,  s,     s, -s,  s,     s,  s,  s,    -s,  s,  s,
+        -s, -s, -s,    -s,  s, -s,     s,  s, -s,     s, -s, -s,
+        -s,  s, -s,    -s,  s,  s,     s,  s,  s,     s,  s, -s,
+        -s, -s, -s,     s, -s, -s,     s, -s,  s,    -s, -s,  s,
+         s, -s, -s,     s,  s, -s,     s,  s,  s,     s, -s,  s,
+        -s, -s, -s,    -s, -s,  s,    -s,  s,  s,    -s,  s, -s
+      ], [ //  vertex indices
+         0,  1,  2,  0,  2,  3, 
+         4,  5,  6,  4,  6,  7, 
+         8,  9, 10,  8, 10, 11, 
+        12, 13, 14, 12, 14, 15,
+        16, 17, 18, 16, 18, 19, 
+        20, 21, 22, 20, 22, 23  
+      ], [ // normals
+         0,  0,  1,     0,  0,  1,     0,  0,  1,     0,  0,  1,
+         0,  0, -1,     0,  0, -1,     0,  0, -1,     0,  0, -1,
+         0,  1,  0,     0,  1,  0,     0,  1,  0,     0,  1,  0,
+         0, -1,  0,     0, -1,  0,     0, -1,  0,     0, -1,  0,
+         1,  0,  0,     1,  0,  0,     1,  0,  0,     1,  0,  0,
+        -1,  0,  0,    -1,  0,  0,    -1,  0,  0,    -1,  0,  0,
+      ], textureURL, [ // texture coords
+         0,  0,         1,  0,         1,  1,         0,  1,
+         1,  0,         1,  1,         0,  1,         0,  0,
+         0,  1,         0,  0,         1,  0,         1,  1,
+         1,  1,         0,  1,         0,  0,         1,  0,
+         1,  0,         1,  1,         0,  1,         0,  0,
+         0,  0,         1,  0,         1,  1,         0,  1
+      ], animation, lighting);
+}
+
+Cube.prototype = Object.create(ItemElements.prototype);
+Cube.prototype.constructor = Cube;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// celestial stuff
+
+
+/**
+ * @super   ItemElements
+ * @param   size           number diameter
+ * @param   textureURL     @see ItemElements
+ * @param   lighting       @see ItemElements
+ */
+function Rings(size, textureURL, lighting) {
+    var s = size / 2;
+
+   ItemElements.call(this, [ // vertices
+        -s,  0, -s,    -s,  0,  s,     s,  0,  s,     s,  0, -s,
+        -s,  0, -s,     s,  0, -s,     s,  0,  s,    -s,  0,  s,
+      ], [ //  vertex indices
+         0,  1,  2,  0,  2,  3, 
+         4,  5,  6,  4,  6,  7, 
+      ], [ // normals
+         0,  1,  0,     0,  1,  0,     0,  1,  0,     0,  1,  0,
+         0, -1,  0,     0, -1,  0,     0, -1,  0,     0, -1,  0,
+      ], textureURL, [ // texture coords
+         0,  1,         0,  0,         1,  0,         1,  1,
+         1,  1,         0,  1,         0,  0,         1,  0,
+      ], function animation() {
+         if (typeof this.animation.day === 'undefined') {
+            this.animation.day = 0;
+         }
+
+         if (!paused) {
+            this.animation.day += 100 * .001;
+         }
+
+
+         mat4.rotate(mvMatrix, mvMatrix, this.axis, [1, 0, 0]);
+         mat4.rotate(mvMatrix, mvMatrix, this.animation.day, [0, 1, 0]);
+
+      }, lighting);  
+}
+
+Rings.prototype = Object.create(ItemElements.prototype);
+Rings.prototype.constructor = Rings;
+
+
+Rings.prototype.draw = function() {
+   //this.lighting();
+   this.animation();
+
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferVertices);
+   gl.vertexAttribPointer(program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufferVertexIndices);
+
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferNormals);
+   gl.vertexAttribPointer(program.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+   
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferTextureCoord);
+   gl.vertexAttribPointer(program.textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+   gl.activeTexture(gl.TEXTURE0);
+   gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
+   // rings use alpha
+   gl.enable(gl.BLEND);
+   gl.blendFunc(gl.SRC_ALPHA, gl.ONE);      
+
+   setMatrixUniforms();
+   gl.drawElements(gl.TRIANGLES, this.vertexIndices.length, gl.UNSIGNED_SHORT, 0);
+
+   gl.disable(gl.BLEND);
+}
+
+
+/**
+ * @super   Sphere 
+ * @param   radius         @see Sphere
+ * @param   textureURL     @see ItemElements
+ * @param   yearLength     number length of time for the planet to orbit origin (arbitrary)
+ * @param   satellites     array of Planets 'owned' by a star
+ */
+function Star(radius, textureURL, daysPerYear, satellites) {
+   this.satellites = satellites;
+   
+   (CUBIVERSE ? Cube : Sphere).call(this, radius, textureURL, function animation() {
+         if (typeof this.animation.day === 'undefined') {
+            this.animation.day = 0;
+         }
+    
+         if (!paused) {
+            this.animation.day += daysPerYear * .001;
+         }
+
+         mvMatrixPush();
+   
+         mat4.rotate(mvMatrix, mvMatrix, this.animation.day, [0, 1, 0]);
+
+      }, function lighting() {
+         gl.uniform3f(program.materialEmissiveColorUniform, .5, .5, 0);
+         gl.uniform1f(program.materialShininessUniform, 5);
+      });
+}
+
+Star.prototype = Object.create(Sphere.prototype);
+Star.prototype.constructor = Star;
+
+
+/**
+ * @super   Sphere 
+ * @param   radius         @see Sphere
+ * @param   textureURL     @see ItemElements
+ * @param   distance       number as the distance from the star/planet it is 'owned' by
+ * @param   eccentricity   number describing the eccentricity of the orbit (circle is 0)
+ * @param   axis           number radians angle for the planet to spin at
+ * @param   yearLength     number length of time for the planet to orbit origin (arbitrary)
+ * @param   daysPerYear    number of days in that year
+ * @param   rings          @see Rings
+ * @param   satellites     array of Planets 'owned' by a planet
+ */
+function Planet(radius, textureURL, distance, eccentricity, movementY, yearLength, daysPerYear, axis, rings, satellites) {
+  
+   if (rings) {
+      this.rings = rings;
+      this.rings.axis = axis;
+   }
+   
+   this.satellites = satellites;   
+   
+   (CUBIVERSE ? Cube : Sphere).call(this, radius, textureURL, function animation() {
+         if (typeof this.animation.year === 'undefined') {
+            this.animation.day = 0;
+            this.animation.year = 0;     
+            this.animation.currentDistance = distance;
+         }
+
+         if (!paused) {
+            var velocity = yearLength * .00001;
+
+            // kepler's first law
+            this.animation.currentDistance = 
+               (distance * (1 + eccentricity)) / 
+               (1 + (eccentricity * Math.cos(this.animation.year)));
+
+            // kepler's second law
+            this.animation.year += (FRAMETIME * distance * distance * velocity) /
+               (this.animation.currentDistance * this.animation.currentDistance);               
+
+            this.animation.day += daysPerYear * .001;
+         } 
+
+         mat4.translate(mvMatrix, mvMatrix, [
+            this.animation.currentDistance * Math.sin(this.animation.year), 
+            movementY * Math.sin(this.animation.year % (2 * Math.PI)), 
+            this.animation.currentDistance * Math.cos(this.animation.year)]);
+
+
+         // we wouldn't want the spinning of the planet to influence any satellites
+         // popped off when drawing satellites
+         mvMatrixPush();
+
+         // spin axis orientation
+         mat4.rotate(mvMatrix, mvMatrix, axis, [1, 0, 0]);
+         mat4.rotate(mvMatrix, mvMatrix, -this.animation.day, [0, 1, 0]);
+
+      }, function lighting() {        
+         gl.uniform3f(program.materialEmissiveColorUniform, 0, 0, 0);         
+         gl.uniform1f(program.materialShininessUniform, 5);  
+      });
+}
+
+Planet.prototype = Object.create(Sphere.prototype);
+Planet.prototype.constructor = Planet;
+
+
+/**
+ * no point adding another layer of inheritance just to stick this in
+ * stars & planets share the ability to have rings/satellites, draws them recursively
+ */
+Planet.prototype.draw = Star.prototype.draw = function() {
+   this.lighting();
+   this.animation();
+
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferVertices);
+   gl.vertexAttribPointer(program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufferVertexIndices);
+
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferNormals);
+   gl.vertexAttribPointer(program.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+   
+   gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferTextureCoord);
+   gl.vertexAttribPointer(program.textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+   gl.activeTexture(gl.TEXTURE0);
+   gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
+   setMatrixUniforms();
+   gl.drawElements(gl.TRIANGLES, this.vertexIndices.length, gl.UNSIGNED_SHORT, 0);
+
+   // removing the star's/planet's spin
+   mvMatrixPop();
+
+   if (this.rings) {
+      mvMatrixPush();
+      this.rings.draw();  
+      mvMatrixPop();
+   }
+
+   for (var i = 0; i < this.satellites.length; i++) {
+      mvMatrixPush();
+      this.satellites[i].draw();
+      mvMatrixPop();
+   }
+}
+
---
+++ src/13/js/main.js
@@ -0,0 +1,280 @@
+/**
+ * @author (gij2) gideon mw jones
+ * @created 2014-10-15
+ */
+
+'use strict';
+
+
+// frameworks
+var gl, 
+   program,
+   mvMatrix = mat4.create(), 
+   mvMatrixStack = [],
+   pMatrix = mat4.create(), 
+   pMovement = { // default mouse position assumes the middle of the screen
+      x : .5, 
+      y : .5,
+      z : .5,
+      scroll : -50
+   },
+   paused = false;
+
+// constants
+var SHADERPATH = 'shader/',
+   TEXTUREPATH = 'texture/',
+   DATAPATH = 'data/',
+   FRAMETIME = 1000 / 60,
+   CUBIVERSE = 0; // 'minecraft mode'
+
+// content
+var items = [];
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// initialisation
+
+var old = [];
+window.onload = function initialise() {
+   var can = document.getElementById('can'), 
+      loop = { 
+         handle : null,
+         func : function tick() {
+            requestAnimationFrame(drawScene);
+         },
+      };
+
+   initialiseGL(can);
+   initialiseProgram(SHADERPATH + 'per-frag-frag.gsgl', SHADERPATH + 'per-frag-vert.gsgl');
+
+   gl.clearColor(0, 0, 0, 1);
+   gl.enable(gl.DEPTH_TEST);
+
+   gl.useProgram(program);
+   initialiseLighting();
+   
+   initialiseControls(can, loop);
+
+   initialiseItems(DATAPATH + 'solarsystem.json');
+
+   loop.handle = setInterval(loop.func, FRAMETIME);
+}
+
+
+
+/**
+ * @param   can            canvas dom object
+ */
+function initialiseGL(can) {
+   try {
+      gl = can.getContext('experimental-webgl');
+
+      can.width = window.innerWidth;
+      can.height = window.innerHeight;
+      gl.width = window.innerWidth;
+      gl.height = window.innerHeight;
+
+   } catch (exeception) {
+      console.error('could not initialise webgl');
+   } 
+}
+
+
+/**
+ * does what it says on the tin
+ */
+function initialiseProgram(fragmentShaderURL, vertexShaderURL) {
+
+   // getting the shader source
+   function getShader(gl, url, type) {
+      var shader = gl.createShader(type),
+         request = new XMLHttpRequest();
+      request.open('GET', url, false);
+      request.send();
+
+      gl.shaderSource(shader, request.responseText);
+      gl.compileShader(shader);
+
+      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+         throw gl.getShaderInfoLog(shader);
+      }
+
+      return shader; 
+   }
+
+
+   program = gl.createProgram();
+
+   var shaderFragment = getShader(gl, fragmentShaderURL, gl.FRAGMENT_SHADER),
+      shaderVertex = getShader(gl, vertexShaderURL, gl.VERTEX_SHADER);
+      
+   gl.attachShader(program, shaderVertex);
+   gl.attachShader(program, shaderFragment);
+   gl.linkProgram(program);
+
+   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.error('could not initialise shaders');
+   }
+
+   program.vertexPositionAttribute = gl.getAttribLocation(program, 'aVertexPosition');
+   gl.enableVertexAttribArray(program.vertexPositionAttribute);
+
+   program.vertexNormalAttribute = gl.getAttribLocation(program, 'aVertexNormal');
+   gl.enableVertexAttribArray(program.vertexNormalAttribute);
+
+   program.textureCoordAttribute = gl.getAttribLocation(program, 'aTextureCoord');
+   gl.enableVertexAttribArray(program.textureCoordAttribute);
+
+   // linking shader variables
+   program.pMatrixUniform = gl.getUniformLocation(program, 'uPMatrix');
+   program.mvMatrixUniform = gl.getUniformLocation(program, 'uMVMatrix');
+   program.nMatrixUniform = gl.getUniformLocation(program, 'uNMatrix');
+   program.samplerUniform = gl.getUniformLocation(program, 'uSampler');
+   program.ambientColorUniform = gl.getUniformLocation(program, 'uAmbientColor');
+   program.materialShininessUniform = gl.getUniformLocation(program, "uMaterialShininess");
+   program.materialEmissiveColorUniform = gl.getUniformLocation(program, 'uMaterialEmissiveColor');
+   program.pointLightingLocationUniform = gl.getUniformLocation(program, 'uPointLightingLocation');
+   program.pointLightingDiffuseColorUniform = gl.getUniformLocation(program, 'uPointLightingDiffuseColor');
+   program.pointLightingSpecularColorUniform = gl.getUniformLocation(program, 'uPointLightingSpecularColor');
+}
+
+
+/**
+ * @param   url            string url of json file describing items 
+ */ 
+function initialiseItems(url) {
+   var request = new XMLHttpRequest();
+
+   request.open('GET', url, false);
+   request.send();
+
+   // recursively generates a list of planets/stars with their satellites from json
+   items = (function generateItems(json) {
+      var result = [];
+
+      for (var i = 0; i < json.length; i++) {
+         var current = json[i], rings;
+
+         var rings = current.rings ? new Rings(current.rings.size, TEXTUREPATH + current.rings.textureURL) : null;
+         
+
+         result[i] = current.type === 'Star' ? 
+            new Star(current.radius, TEXTUREPATH + current.textureURL, current.daysPerYear, 
+               generateItems(current.satellites)) :
+            new Planet(current.radius, TEXTUREPATH + current.textureURL, current.distance, 
+               current.eccentricity, current.movementY, current.velocity, current.daysPerYear,
+               current.axis, rings, generateItems(current.satellites));
+      }
+
+      return result;
+   })(JSON.parse(request.responseText));
+}
+
+
+function initialiseControls() {
+   document.addEventListener('mousemove', rotatePerspective, false);
+   document.addEventListener('mousewheel', zoomPerspective, false);
+   document.addEventListener('DOMMouseScroll', zoomPerspective, false);
+   document.addEventListener('keypress', togglePause, false);
+   
+/*   window.addEventListener('deviceorientation', rotatePerspectiveAlt, true);
+   function rotatePerspectiveAlt(eve) {
+      pMovement.x = eve.gamma / 90;
+      pMovement.y = -eve.beta / 90;
+      pMovement.z = -eve.alpha / 90;
+      console.log(eve);
+   }*/
+
+   // perspective shifting
+   function rotatePerspective(eve) {
+      pMovement.x = eve.clientX / can.width;
+      pMovement.y = eve.clientY / can.height;
+   }
+
+   // zooming
+   function zoomPerspective(eve) {
+      eve.preventDefault();
+      pMovement.scroll += eve.wheelDelta  > 0 || eve.detail < 0 ? .3 : -.3;
+   }
+
+   // toggles the pausing of the program
+   function togglePause(eve) {    
+       switch (eve.keyCode) {
+         case 32 :
+            paused = !paused;
+            break;
+      }
+   }
+}
+
+
+function initialiseLighting() {
+   gl.uniform3f(program.pointLightingLocationUniform,  0, 0, 0);
+   gl.uniform3f(program.pointLightingDiffuseColorUniform, .5, .5, .5);
+   gl.uniform3f(program.pointLightingSpecularColorUniform, .5, .5, .5);
+   gl.uniform1i(program.samplerUniform, 0);
+   gl.uniform3f(program.ambientColorUniform, .1, .1, .1);  
+   gl.uniform3f(program.materialEmissiveColorUniform, 0, 0, 0);
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// animation
+
+
+/**
+ * assembles & draws the items of the scenr
+ */
+function drawScene() {
+   gl.viewport(0, 0, gl.width, gl.height);
+   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+   mat4.perspective(pMatrix, 45, gl.width / gl.height, .1, 200);
+   mat4.translate(pMatrix, pMatrix, [0, 0, pMovement.scroll])
+
+   // moving the perspective based on cursor location
+   mat4.rotate(pMatrix, pMatrix, pMovement.x - .5, [0, 1, 0]);
+   mat4.rotate(pMatrix, pMatrix, pMovement.y - .5, [1, 0, 0]);
+   //   mat4.rotate(pMatrix, pMatrix, pMovement.z, [0, 0, 1]);
+
+
+
+   // basic order of things is - move the origin via a series of matrices, draw, then reset origin
+   for (var i = 0; i < items.length; i++) {
+      mvMatrixPush();
+      items[i].draw();
+      mvMatrixPop();
+   }
+
+   for (var i = 0; i < items.length; i++) {
+
+   }
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// tools
+
+
+function setMatrixUniforms() {
+   gl.uniformMatrix4fv(program.pMatrixUniform, false, pMatrix);
+   gl.uniformMatrix4fv(program.mvMatrixUniform, false, mvMatrix);
+ 
+   var nMatrix = mat3.create();
+   mat3.normalFromMat4(nMatrix, mvMatrix);  
+   gl.uniformMatrix3fv(program.nMatrixUniform, false, nMatrix);
+}
+
+
+function mvMatrixPush() {
+   var copy = mat4.create();
+   mat4.copy(copy, mvMatrix);
+   mvMatrixStack.push(copy);
+}
+
+
+function mvMatrixPop() {
+   mvMatrix = mvMatrixStack.pop();
+}
+
